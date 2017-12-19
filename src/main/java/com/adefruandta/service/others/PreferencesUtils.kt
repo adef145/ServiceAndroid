1420e3d4deb79cebdd39da94f5137d2c0127054a
Ade Fruandta <ade.fruandta@gmail.com>
2017-12-19 11:08:48 +0700

    rename extension

--- src/main/java/com/adefruandta/service/others/PreferencesUtils.kt
+++ src/main/java/com/adefruandta/service/others/PreferencesUtils.kt
@@ -6,13 +6,13 @@ import android.content.SharedPreferences
  * Created by adefruandta on 12/14/17.
  */
 
-fun SharedPreferences.commit(listener: (SharedPreferences.Editor) -> Unit) {
+fun SharedPreferences.editorCommit(listener: (SharedPreferences.Editor) -> Unit) {
     val editor = edit()
     listener(editor)
     editor.commit()
 }
 
-fun SharedPreferences.apply(listener: (SharedPreferences.Editor) -> Unit) {
+fun SharedPreferences.editorApply(listener: (SharedPreferences.Editor) -> Unit) {
     val editor = edit()
     listener(editor)
     editor.apply()
